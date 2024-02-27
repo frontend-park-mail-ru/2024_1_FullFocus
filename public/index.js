61d43d20c18651a43f0cc2161378c02a540a2161
Bopster411 <serg.dr2004@gmail.com>
2024-02-28 00:06:10 +0300

    Added ajax
    Added authentication, registration and main page feed

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
-console.log('epic');
-
+// Init
 const rootElement = document.getElementById('root');
 const menuElement = document.createElement('header');
 const contentElement = document.createElement('main');
@@ -12,7 +11,7 @@ const config = {
         main: {
             href: '/',
             text: 'Ozon',
-            render: renderDefault
+            render: renderMain
         },
         login: {
             href: '/login',
@@ -27,31 +26,36 @@ const config = {
         profile: {
             href: '/me',
             text: 'Профиль',
-            render: renderDefault
+            render: renderProfile
         }
     }
 };
 
 const state = {
     activeMenuItem: null,
+    menuElements: {},
 };
 
-Object
-    .entries(config.menu)
-    .forEach(([key, {href, text}], index) => {
-        const menuItemElement = document.createElement('a');
-        menuItemElement.href = href;
-        menuItemElement.textContent = text;
-        menuItemElement.dataset.section = key;
 
-        if (index === 0) {
-            menuItemElement.classList.add('active');
-            state.activeMenuItem = menuItemElement;
-        }
+// Helpful functions
+function ajax(method, url, body = null, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.withCredentials = true;
 
-        menuElement.appendChild(menuItemElement);
-    });
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState != XMLHttpRequest.DONE) return;
+        callback(xhr.status, xhr.responseText);
+    })
 
+    if (body) {
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf8');
+        xhr.send(JSON.stringify(body));
+        return;
+    }
+
+    xhr.send();
+}
 
 function createInput(type, text, name) {
     const input = document.createElement('input');
@@ -60,6 +64,48 @@ function createInput(type, text, name) {
     input.placeholder = text;
 
     return input;
+}
+
+function goToPage(menuElement) {
+    contentElement.innerHTML = '';
+
+    state.activeMenuItem.classList.remove('active');
+    menuElement.classList.add('active');
+    state.activeMenuItem = menuElement;
+
+    const element = config.menu[menuElement.dataset.section].render();
+    contentElement.appendChild(element);
+}
+
+// Render functions
+function renderMenu() {
+    Object
+        .entries(config.menu)
+        .forEach(([key, { href, text }], index) => {
+            const menuItemElement = document.createElement('a');
+            menuItemElement.href = href;
+            menuItemElement.textContent = text;
+            menuItemElement.dataset.section = key;
+
+            if (index === 0) {
+                menuItemElement.classList.add('active');
+                state.activeMenuItem = menuItemElement;
+            }
+
+            menuElement.appendChild(menuItemElement);
+
+            state.menuElements[key] = menuItemElement;
+        });
+    
+    menuElement.addEventListener('click', (e) => {
+        const { target } = e;
+
+        if (target.tagName.toLowerCase() === 'a') {
+            e.preventDefault();
+
+            goToPage(e.target);
+        }
+    })
 }
 
 function renderLogin() {
@@ -76,11 +122,25 @@ function renderLogin() {
     form.appendChild(passwordInput);
     form.appendChild(submitBtn);
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
+
+        ajax('POST', '/login', {password: password, email: email}, (status) => {
+            if (status === 200) {
+                goToPage(state.menuElements.profile);
+                return;
+            }
+
+            alert('error');
+        })
+    })
+
     return form;
 }
 
 function renderSignup() {
-    
     const form = document.createElement('form');
 
     const emailInput = createInput('email', 'Почта', 'email');
@@ -96,7 +156,64 @@ function renderSignup() {
     form.appendChild(ageInput);
     form.appendChild(submitBtn);
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
+        const age = ageInput.value.trim();
+
+        ajax('POST', '/signup', {email: email, password: password, age: age}, (status) => {
+            if (status === 200) {
+                goToPage(state.menuElements.profile);
+                return;
+            }
+
+            alert('error');
+        })
+    })
+
     return form;
+}
+
+function renderProfile() {
+    const profileElement = document.createElement('div');
+
+    ajax('GET', '/me', null, (status, responseString) => {
+        const isAuthorized = status === 200;
+
+        if (!isAuthorized) {
+            goToPage(state.menuElements.login);
+            return;
+        }
+
+        const { email, age } = JSON.parse(responseString);
+        const span = document.createElement('span');
+        span.textContent = `Почта ${email} возраст ${age}`;
+
+        profileElement.appendChild(span);
+    })
+
+    return profileElement;
+}
+
+function renderMain() {
+    const feedElement = document.createElement('div');
+
+    ajax('GET', '/feed', null, (status, responseString) => {
+        const products = JSON.parse(responseString);
+        if (products && Array.isArray(products)) {
+            const div = document.createElement('div');
+            feedElement.appendChild(div);
+
+            products.forEach(({name, cost}) => {
+                const prodPara = document.createElement('p');
+                prodPara.innerText = `${name} - ${cost} руб.`;
+                div.appendChild(prodPara);
+            })
+        }
+    })
+
+    return feedElement;
 }
 
 function renderDefault() {
@@ -104,19 +221,5 @@ function renderDefault() {
     return container;
 }
 
-menuElement.addEventListener('click', (e) => {
-    const {target} = e;
-
-    if (target.tagName.toLowerCase() === 'a') {
-        e.preventDefault();
-        
-        contentElement.innerHTML = '';
-
-        state.activeMenuItem.classList.remove('active');
-        e.target.classList.add('active');
-        state.activeMenuItem = e.target;
-
-        const element = config.menu[target.dataset.section].render();
-        contentElement.appendChild(element);
-    }
-})
+renderMenu();
+goToPage(state.menuElements.main)
