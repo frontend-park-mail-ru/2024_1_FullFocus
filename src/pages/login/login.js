615c3d46bd5fcfe9447738fcad074136892d0908
Bopster411 <serg.dr2004@gmail.com>
2024-03-10 14:41:24 +0300

    Added validation to forms

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,7 @@
 import { ajaxMultipartForm } from "../../shared/api/ajax";
 import { LoginForm } from "../../widgets/loginForm/loginForm";
 import { EmptyContainer } from "../../shared/uikit/emptyContainer/emptyContainer";
+import { validateLogin, validatePassword } from "../../shared/lib/validate/validate";
 
 export class Login {
     /**
@@ -29,15 +30,42 @@ export class Login {
         this.htmlElement.addEventListener('submit', (e) => {
             e.preventDefault();
             this.errorsElement.textContent = '';
-            this.formEl = document.getElementsByClassName('login-form')[0];
 
-            ajaxMultipartForm('POST', '/api/auth/login', this.formEl, (status, msg, msgrus) => {
-                if (status === 200) {
-                    this.parentItem.goToPage('main');
-                    return;
-                }
-                this.errorsElement.textContent = msgrus;
-            })
+            const loginField = this.formObj.form.getFieldByName('login');
+            
+            let error = validateLogin(loginField.value);
+            let isValid = true;
+            if (error != null) {
+                isValid = false;
+                loginField.classList.add('invalid');
+                this.formObj.form.getErrorFieldByInputName('login').textContent = error;
+            } else {
+                loginField.classList.remove('invalid');
+                loginField.classList.add('valid');
+                this.formObj.form.getErrorFieldByInputName('login').textContent = '';
+            }
+            
+            const passwordField = this.formObj.form.getFieldByName('password');
+            error = validatePassword(passwordField.value);
+            if (error != null) {
+                isValid = false;
+                passwordField.classList.add('invalid');
+                this.formObj.form.getErrorFieldByInputName('password').textContent = error;
+            } else {
+                passwordField.classList.remove('invalid');
+                loginField.classList.add('valid');
+                this.formObj.form.getErrorFieldByInputName('password').textContent = '';
+            }
+
+            if (isValid) {
+                ajaxMultipartForm('POST', '/api/auth/login', this.formEl, (status, msg, msgrus) => {
+                    if (status === 200) {
+                        this.parentItem.goToPage('main');
+                        return;
+                    }
+                    this.errorsElement.textContent = msgrus;
+                })
+            }
         })
 
         return this.htmlElement;
