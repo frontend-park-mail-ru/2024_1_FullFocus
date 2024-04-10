0fc1b8c7a5686f0b1b25e195221bda3c06bc1a23
Bopster411 <serg.dr2004@gmail.com>
2024-04-10 14:41:06 +0300

    fixes

--- src/entities/product/api/index.ts
+++ src/entities/product/api/index.ts
@@ -4,17 +4,23 @@ import { PRODUCTS_API_URL } from './index.constants';
 
 export { IProductResponse } from './index.types';
 
-export async function productsRequest(lastId: number, limit: number) {
-    return ajaxGet<Array<IProductResponse>>(PRODUCTS_API_URL.getProducts, [
-        { key: 'lastid', value: lastId.toString() },
-        { key: 'limit', value: limit.toString() },
-    ]);
+export async function productsRequest(page: number, limit: number) {
+    return ajaxGet<{ productCards: Array<IProductResponse> }>(
+        PRODUCTS_API_URL.getProducts,
+        [
+            { key: 'page', value: page.toString() },
+            { key: 'limit', value: limit.toString() },
+        ],
+    );
 }
 
 // TODO move to productssection?
 export async function productsRequestCategory(categoryId: number) {
     return ajaxGet<IProductResponse[]>(
         PRODUCTS_API_URL.getProductsCategory + categoryId.toString(),
-        [],
+        [
+            { key: 'page', value: '1' },
+            { key: 'limit', value: '10' },
+        ],
     );
 }
